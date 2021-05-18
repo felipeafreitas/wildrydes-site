3c8cebf1189ff473ee942164439e58242eeaac34
Felipe Freitas <afelipe.freitas@gmail.com>
2021-05-17 21:44:03 -0300

    Part 2 Done

--- src/config.js
+++ src/config.js
@@ -1,7 +1,13 @@
-// Add your API invoke URL below, configured in Module 3. 
+// Add your API invoke URL below, configured in Module 3.
 
 module.exports = {
-    api: {
-        invokeUrl: '' // e.g. https://rc2345678345.execute-api.us-west-2.amazonaws.com/prod,
-    }
-}
+  cognito: {
+    userPoolId: "us-east-2_sJG39lTrM", // e.g. us-east-2_uXboG5pAb
+    userPoolClientId: "gd1j8823v4afq0r0nck7e9ocn", // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+    region: "us-east-2", // e.g. us-east-2
+  },
+
+  api: {
+    invokeUrl: "", // e.g. https://rc2345678345.execute-api.us-west-2.amazonaws.com/prod,
+  },
+};
